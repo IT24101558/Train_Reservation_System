72947d78bd0455c3420c89ad449d50d80d350909
SanithTW <sanithwijesinghe30@gmail.com>
2025-08-23 13:04:48 +0530

    project

---
+++ project/src/main/java/__Y2_S1_MTR_02/project/WebBasedTrainReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package __Y2_S1_MTR_02.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBasedTrainReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBasedTrainReservationSystemApplication.class, args);
+	}
+
+}
---
+++ project/src/test/java/__Y2_S1_MTR_02/project/WebBasedTrainReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package __Y2_S1_MTR_02.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBasedTrainReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
