1e2eafaa9f249a56da4d6c0cbf2d1999398ff9da
Soesh Cooray <it24101009@my.sliit.lk>
2025-09-23 10:17:48 +0530

    Added feedback controller

---
+++ project/src/main/java/__Y2_S1_MTR_02/controller/FeedbackController.java
@@ -0,0 +1,33 @@
+package __Y2_S1_MTR_02.controller;
+
+import __Y2_S1_MTR_02.dto.FeedbackDTO;
+import __Y2_S1_MTR_02.service.FeedbackService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/feedback")
+public class FeedbackController {
+
+    @Autowired
+    private FeedbackService feedbackService;
+
+    @GetMapping
+    public ResponseEntity<List<FeedbackDTO>> getAllFeedbacks() {
+        return ResponseEntity.ok(feedbackService.getAllFeedbacks());
+    }
+
+    @PostMapping
+    public ResponseEntity<FeedbackDTO> saveFeedback(@RequestBody FeedbackDTO feedbackDTO) {
+        return ResponseEntity.ok(feedbackService.saveFeedback(feedbackDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteFeedback(@PathVariable Long id) {
+        feedbackService.deleteFeedback(id);
+        return ResponseEntity.noContent().build();
+    }
+}
