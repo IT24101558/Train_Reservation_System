455dbaab4885110dd53d5e2e4290b90b6762e727
Soesh Cooray <it24101009@my.sliit.lk>
2025-09-23 10:27:14 +0530

    Added feedback backend

---
+++ project/src/main/java/__Y2_S1_MTR_02/model/Feedback.java
@@ -0,0 +1,72 @@
+package __Y2_S1_MTR_02.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Feedback {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String passengerName;
+
+    private String email;
+
+    private int rating;
+
+    @Column(length = 1000)
+    private String message;
+
+    private LocalDateTime dateTime;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPassengerName() {
+        return passengerName;
+    }
+
+    public void setPassengerName(String passengerName) {
+        this.passengerName = passengerName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+}
