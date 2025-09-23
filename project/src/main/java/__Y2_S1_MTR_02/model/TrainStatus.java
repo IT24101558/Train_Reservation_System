330aebc9048adfaaecb5bd4e14efb25a6cf47564
Soesh Cooray <it24101009@my.sliit.lk>
2025-09-23 10:28:29 +0530

    Update imports

---
+++ project/src/main/java/__Y2_S1_MTR_02/model/TrainStatus.java
@@ -0,0 +1,5 @@
+0package __Y2_S1_MTR_02.model;
+
+public enum TrainStatus {
+    ACTIVE, DELAYED, CANCELED, MAINTENANCE
+}
--- project/src/main/java/__Y2_S1_MTR_02/model/TrainType.java
+++ project/src/main/java/__Y2_S1_MTR_02/model/TrainType.java
@@ -1,5 +1,5 @@
 // src/main/java/com/_Y2_S1_MTR_02/model/TrainType.java
-package _Y2_S1_MTR_02.model;
+package __Y2_S1_MTR_02.model;
 
 public enum TrainType {
     EXPRESS,
