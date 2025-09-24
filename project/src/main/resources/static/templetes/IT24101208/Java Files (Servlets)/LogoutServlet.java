276ab08b908574adbae57bf35b013a017f922a0f
IT24300073 <IT24300073@my.sliit.lk>
2025-09-24 11:07:49 +0530

    Removed one nethi files

--- project/src/main/resources/static/templetes/IT24101208/Java Files (Servlets)/LogoutServlet.java
+++
@@ -1,8 +0,0 @@
-@WebServlet("/Logout")
-public class LogoutServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws IOException {
-        request.getSession().invalidate();
-        response.sendRedirect("HTML/login.jsp");
-    }
-}
