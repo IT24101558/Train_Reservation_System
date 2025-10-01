96b401b3eaabbaabf7157b145309d861014db44a
IT24101525 <it24101525@my.sliit.lk>
2025-10-02 00:37:40 +0530

    WebConfig.java

--- project/src/main/java/__Y2_S1_MTR_02/config/WebConfig.java
+++ project/src/main/java/__Y2_S1_MTR_02/config/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -14,7 +15,8 @@ import java.util.concurrent.TimeUnit;
 public class WebConfig implements WebMvcConfigurer {
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    @Primary
+    public PasswordEncoder appPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
