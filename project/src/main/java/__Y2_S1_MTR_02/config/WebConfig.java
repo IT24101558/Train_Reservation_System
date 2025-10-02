cff2f728be7de08a32ebc5906ebf8ae07c91fadc
Soesh Cooray <it24101009@my.sliit.lk>
2025-10-02 19:16:38 +0530

    Updated the database

--- project/src/main/java/__Y2_S1_MTR_02/config/WebConfig.java
+++ project/src/main/java/__Y2_S1_MTR_02/config/WebConfig.java
@@ -1,27 +1,17 @@
 package __Y2_S1_MTR_02.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.context.annotation.Primary;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.http.CacheControl;
-import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    @Primary
-    public PasswordEncoder appPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
         // Serve static resources from the static directory
         registry.addResourceHandler("/templetes/**")
                 .addResourceLocations("classpath:/static/templetes/")
@@ -30,7 +20,7 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
